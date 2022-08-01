2e3be22693a19e94056938dfc7668872308730ba
Fhellipy <fhellipy.santana@dantaseletro.com.br>
2022-08-01 16:19:59 -0300

    feat: adiciona as imagens de preview de cada projeto

--- src/components/CarouselProject/index.tsx
+++ src/components/CarouselProject/index.tsx
@@ -43,44 +43,43 @@ function CarouselProject() {
     ],
   };
 
-  const logo = "/Logo.jpg";
+  const logoDiscord = "/discord.png";
+  const logoTwitter = "/twitter.jpeg";
+  const logoFake_app = "/fake-app.jpeg";
 
   return (
     <article className={css.container}>
       <Slider className={css.carousel} {...settings}>
         <ul>
           <li>
-            <img src={logo} className={css.image} />
+            <a href="https://github.com/Fhellipy/discord-clone" target="blank">
+              <img src={logoDiscord} className={css.image} />
+              <p className={css.titleProject}>
+                <strong>Clone Discord</strong>
+              </p>
+            </a>
           </li>
         </ul>
 
         <ul>
           <li>
-            <img className={css.image} src={logo} />
+            <a href="https://github.com/Fhellipy/twitter-clone" target="blank">
+              <img src={logoTwitter} className={css.image} />
+              <p className={css.titleProject}>
+                <strong>Clone Twitter</strong>r
+              </p>
+            </a>
           </li>
         </ul>
 
         <ul>
           <li>
-            <img className={css.image} src={logo} />
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <img className={css.image} src={logo} />
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <img className={css.image} src={logo} />
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <img className={css.image} src={logo} />
+            <a href="https://github.com/Fhellipy/fake-app" target="blank">
+              <img src={logoFake_app} className={css.image} />
+              <p className={css.titleProject}>
+                <strong>Fake app</strong>
+              </p>
+            </a>
           </li>
         </ul>
       </Slider>
