79a1b25e00bcab80e7d9bd33b83a6c5ffcc8c683
Fhellipy <fhellipy.santana@dantaseletro.com.br>
2022-08-01 15:26:14 -0300

    feat: adiciona  o icone do usuário

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import css from "./styles.module.css";
 
 function Avatar() {
+  const logo = "/Logo.jpg";
+
   return (
     <div className={css.container}>
-      <div className={css.photoUser}>{/* <Img /> */}</div>
+      <div className={css.photoUser}>
+        <img src={logo} alt="Icone do usuário" />
+      </div>
     </div>
   );
 }
 
 export { Avatar };
-
-
